8bea7e40f8472aa558e008cadc64b87a175fc571
vagoutha <115881588+vagoutha@users.noreply.github.com>
2022-10-30 23:43:10 +0530

    Labour.java

---
+++ Labour.java
@@ -0,0 +1,11 @@
+package com.company;
+  public class Labour extends Employee {
+    public static final double overtime= 0.5;
+    public Labour(int employeeId, String employeeName, double salary){
+        super(employeeId, employeeName, salary);
+    }
+    public double getSalary() {
+        return salary + salary * overtime;
+    }
+}
+
