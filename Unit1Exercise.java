1169f69e9d119c66e49480134b6130c1958afda4
vagoutha <115881588+vagoutha@users.noreply.github.com>
2022-11-03 13:46:20 +0530

    Unit1Exercise.java

---
+++ Unit1Exercise.java
@@ -0,0 +1,22 @@
+
+import com.company.Person;
+import java.util.Arrays;
+import java.util.List;
+
+public class Unit1Exercise {
+    public static void main(String[] args){
+    List<Person> people =Arrays.asList(
+            new Person("Vanga","Gouthami",25),
+            new Person("Matta","Harshita",30),
+            new Person("Chilusani","Sharishma",35),
+            new Person("Jathothu","Parvati",40),
+            new Person("Vanga","Vinay",19)
+    );
+    //step1: Sort list by last name
+    //step2: Create a method that prints all elelments in the list
+    //step3: Create a method that prints all people that have last name beginning with V
+}
+}
+
+
+
