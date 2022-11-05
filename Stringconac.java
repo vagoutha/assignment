5a5b02942d6aa90bde05c07017357a47ef571667
vagoutha <115881588+vagoutha@users.noreply.github.com>
2022-11-05 23:09:41 +0530

    Add files via upload

---
+++ Stringconac.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.*;
+public class Stringconac {
+    public static void main(String[] args) {
+        String a="Hello";
+        String b=" How are you?";
+        String c=a.concat(b);
+        System.out.println(c);
+    }
+}
