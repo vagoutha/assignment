7ec8627db2a8bac004ff28c2ed05e22b775af4bc
vagoutha <115881588+vagoutha@users.noreply.github.com>
2022-11-05 15:50:35 +0530

    Add files via upload

---
+++ LambdaQue4.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class LambdaQue4 {
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<String>(Arrays.asList("hello", "Gouthami", "good", "morning", "zoom"));
+        System.out.println(list1);
+        for (String word : list1)
+            System.out.println(word);
+        System.out.println("the words with the even number length:");
+        list1.removeIf(word ->((word.length()%2 )!= 0));
+        for (String word : list1){
+            System.out.println(word);
+        }
+    }
+
+}
