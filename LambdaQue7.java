e4787a9f51d6b3a14fdb191bd1ce9fe10181d660
vagoutha <115881588+vagoutha@users.noreply.github.com>
2022-11-05 15:52:29 +0530

    Add files via upload

---
+++ LambdaQue7.java
@@ -0,0 +1,30 @@
+package com.lambda;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+public class LambdaQue7{
+    public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("a",2);
+        map.put("b",6);
+        map.put("c",8);
+        LambdaQue7 KeyValues = new LambdaQue7();
+        KeyValues.convertKeyValueToString(map);
+    }
+    public String convertKeyValueToString(HashMap<String, Integer> map) {
+
+        StringBuilder str = new StringBuilder();
+        Consumer<Map.Entry> consumer=(p)->str.append(p.getKey()).append(p.getValue());
+        Set set  = map.entrySet();
+        set.stream()
+                .forEach(consumer);
+        System.out.println(str);
+        return str.toString();
+    }
+}
+
+
+
+
