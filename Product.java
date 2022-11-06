f10262be47bd62f4f2b2f78667bb35487913d5e2
vagoutha <115881588+vagoutha@users.noreply.github.com>
2022-11-06 10:48:27 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*;
+
+public class Product {
+    public static void main(String[] args)
+    {
+        HashSet<String> obj=new HashSet<>();
+        obj.add("plank");
+        obj.add("chairs");
+        obj.add("pens");
+        obj.add("table");
+        obj.add("Books");
+        obj.add("papers");
+        obj.add("flank");
+        obj.add("breshes");
+        obj.add("colors");
+        obj.add("mobiles");
+        System.out.println(obj);
+    }
+}
