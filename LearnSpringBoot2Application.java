85cf414749d4046133bed580303d21387b5161b4
vagoutha <115881588+vagoutha@users.noreply.github.com>
2022-11-20 23:08:52 +0530

    Add files via upload

---
+++ LearnSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spingboot.learnspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBoot2Application.class, args);
+	}
+
+}
