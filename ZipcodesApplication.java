97a13e37802d1975cbb4b35267958736ac8a927d
vagoutha <115881588+vagoutha@users.noreply.github.com>
2022-11-20 23:19:04 +0530

    Add files via upload

---
+++ ZipcodesApplication.java
@@ -0,0 +1,13 @@
+package com.assignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipcodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipcodesApplication.class, args);
+	}
+
+}
