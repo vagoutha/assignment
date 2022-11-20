0ce082a8f27d26253ab9167e58e143c455fd47c3
vagoutha <115881588+vagoutha@users.noreply.github.com>
2022-11-20 23:06:55 +0530

    Add files via upload

---
+++ CourseController.java
@@ -0,0 +1,26 @@
+package com.in28minutes.spingboot.learnspringboot3;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+
+	@RequestMapping("/courses")
+
+	public List<Course> retriveAllCourses() {
+		return Arrays.asList(
+
+				new Course(1,"Learn AWS" , "in28minutes"),
+				new Course(2,"Learn Devops","in28minutes"),
+				new Course(3,"Learn Azure","in28minutes")
+
+
+
+				);
+	}
+	}
---
+++ LearnSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spingboot.learnspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBoot3Application.class, args);
+	}
+
+}
